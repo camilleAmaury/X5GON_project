40f1cd65c93206349ac0aa4e41d2550a10c9f872
Torilen <saeran.ion@gmail.com>
2020-01-13 10:18:09 +0000

    like difficulty endpoint

--- BackEnd/endpoints/__init__.py
+++ BackEnd/endpoints/__init__.py
@@ -5,6 +5,8 @@ from .askquestion import api as askquestion_api
 from .prerequisites import api as prerequisites_api
 from .register import api as register_api
 from .login import api as login_api
+from .like import api as like_api
+from .difficulty import api as difficulty_api
 
 api = Api(
     title='X5GON API',
@@ -17,4 +19,6 @@ api.add_namespace(askquestion_api)
 api.add_namespace(prerequisites_api)
 api.add_namespace(login_api)
 api.add_namespace(register_api)
+api.add_namespace(like_api)
+api.add_namespace(difficulty_api)
 
