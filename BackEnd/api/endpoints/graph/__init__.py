b14f5580ee7cc23e3ea5174ffc2a7fdb6c2ac856
kovinns <kovinns@hotmail.fr>
2020-02-22 22:58:04 +0100

    Local execution modification

--- BackEnd/api/endpoints/graph/__init__.py
+++ BackEnd/api/endpoints/graph/__init__.py
@@ -1,6 +1,6 @@
 import nltk
 from nltk.corpus import stopwords
-from transformers import BertTokenizer, BertForQuestionAnswering
+# from transformers import BertTokenizer, BertForQuestionAnswering
 import en_core_web_sm
 from .fastTextVectors import vectors
 
@@ -13,7 +13,7 @@ vec = None
 if __name__ == "__main__":
 
     stop_words = set(stopwords.words('english'))
-    question_answering_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    question_answering_model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+    # question_answering_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    # question_answering_model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
     nlp = en_core_web_sm.load()
     vec = vectors
