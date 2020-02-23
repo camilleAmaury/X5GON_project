aae02d99112a2f48f9b54f015838639486284914
kovinns <kovinns@hotmail.fr>
2020-02-23 15:12:32 +0100

    Revert "Revert "Local execution modification""
    This reverts commit 64fa2b025f5d698a13d3a2e7b99678845bebd686.

--- BackEnd/api/endpoints/graph/__init__.py
+++ BackEnd/api/endpoints/graph/__init__.py
@@ -1,6 +1,6 @@
 import nltk
 from nltk.corpus import stopwords
-from transformers import BertTokenizer, BertForQuestionAnswering
+# from transformers import BertTokenizer, BertForQuestionAnswering
 import en_core_web_sm
 
 stop_words = None
@@ -10,8 +10,8 @@ nlp = None
 
 def initialize_askquestion_variables():
     stop_words = set(stopwords.words('english'))
-    question_answering_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    question_answering_model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+    # question_answering_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    # question_answering_model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
     nlp = en_core_web_sm.load()
 
 def get_stop_words():
