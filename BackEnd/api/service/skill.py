a0b84d32a2885d85bebb75e3431d9cc031cbdb07
Torilen <saeran.ion@gmail.com>
2020-02-24 15:18:12 +0000

    tblabla

--- BackEnd/api/service/skill.py
+++ BackEnd/api/service/skill.py
@@ -2,7 +2,7 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 def build_skills_schema(skills_dictionary):
     arr = []
-    for key, level in skills_dictionary :
+    for key, level in skills_dictionary.items():
         mod = {
             'skill_name' : key,
             'skill_level' : skills_dictionary.get(key)
@@ -25,11 +25,15 @@ def getKeywords(listIdDoc):
         union += "{?doc dcterms:subject ?key; dcterms:identifier "+str(id)+".}"
         union += "UNION"
 
-    union = union[:5]
+    union = union[:-5]
 
     query = "PREFIX x5gonbjk: <http://x5gon/bjk/> PREFIX dcterms: <http://purl.org/dc/terms/> SELECT ?key (COUNT(?key) as ?val) WHERE {"+union+"}GROUP BY ?key ORDER BY DESC(?val) LIMIT 8"
+    
+    print(query)
+    
     res = execQuery(sparql, query)['results']['bindings']
     for i in range(len(res)):
         resultat[res[i]['key']['value']] = res[i]['val']['value']
 
+    print(resultat)
     return resultat
