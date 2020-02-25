e6e4fbcbd456e73bc4ae507574226c41939f4973
Torilen <saeran.ion@gmail.com>
2020-02-25 15:48:00 +0000

    blabla

--- BackEnd/api/endpoints/database/remove_my_data.py
+++ BackEnd/api/endpoints/database/remove_my_data.py
@@ -13,17 +13,13 @@ import numpy
 
 api = Namespace('remove_my_data', description='The endpoint to allow a user to remove all of his data')
 
-doc = api.model('Document', {
-    'id': fields.String(required=True, description='The document identifier'),
-})
 
 @api.route('/<idUser>')
-@api.param('idUser', 'Document identifier')
-@api.response(404, 'Documents not found')
+@api.param('idUser', 'User identifier')
 class remove_my_data(Resource):
     @api.doc('list_id_doc')
     #@api.marshal_list_with(doc)
-    def get(self, idUser):
+    def delete(self, idUser):
         
         engine2 = create_engine('mysql://torilen:kiogre97@localhost:3306/x5gon')
         connection2 = engine2.connect()
